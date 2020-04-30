4fd7e4b9ab5650b3f3a0be4167487429ab307ded
Evandro-FDVO <evandro.della@gmail.com>
2020-04-30 14:30:39 -0300

    Modelo de Usuario

---
+++ src/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');/* Definindo e importando o Mongoose */
+
+
+/* Definindo o Schema: campos do Banco de Dados */
+const UserSchema = new mongoose.Schema({
+
+    nome: {
+        type: String,
+        require: true,
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+
+    },
+
+    senha: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const User = mongoose.model('User', UserSchema); /* Definindo o User  */
+
+module.exports = User;
