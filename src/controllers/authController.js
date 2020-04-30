be39fcc0167ff3d3f811889a297b9b163ed35ef0
Evandro-FDVO <evandro.della@gmail.com>
2020-04-30 15:34:31 -0300

    Encriptação do usuário

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,8 +6,17 @@ const router = express.Router();/* Chamnado função Router do express para defi
 
 /* Rota de cadastro */
 router.post('/register', async (req, res) => {
+    const { email } = req.body;
+
     try {
+
+        /* Conferindo e retornando de o usuario já existe */
+        if (await User.findOne({ email }))
+        return res.status(400).send({ error: 'O usuário já existe!' });
+
         const user = await User.create(req.body);
+
+        user.senha = undefined;/* Escondendo senha */
 
         return res.send({ user });
     } catch (err) {
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database');/* Definindo e importando o Mongoose */
+const bcrypt = require('bcrypt');/* Definindo e importando o Bcrypt */
 
 
 /* Definindo o Schema: campos do Banco de Dados */
@@ -26,6 +27,15 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+});
+
+
+/* Encriptação da Senha */
+UserSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.senha, 10);
+    this.senha = hash;
+
+    next();
 });
 
 const User = mongoose.model('User', UserSchema); /* Definindo o User  */
