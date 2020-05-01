99eca2cc6242bbadb7726dde642a6d1471251cb2
Evandro-FDVO <evandro.della@gmail.com>
2020-05-01 01:37:48 -0300

    Delete

--- src/database/index.js
+++
@@ -1,7 +0,0 @@
-
-const mongoose = require('mongoose');/* Definindo e importando o Mongoose */
-
-mongoose.connect("mongodb://localhost/ctapp", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });/* Conectando com o Banco de Dados */
-mongoose.Promise = global.Promise;/* Definindo e identificando a classe  de promise que o mongoose vai usar*/
-
-module.exports = mongoose;/* Exportando modulo mongoose */
